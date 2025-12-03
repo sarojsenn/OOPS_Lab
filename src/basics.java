e85cf3b7dcd1bc45d473f72d198657166a1d9915
Saroj Sen <sarojsen4952@gmail.com>
2025-12-03 13:04:01 +0530

    updated

--- src/basics.java
+++ src/basics.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-public class basics{
+class basics{
     public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        float c = 3.14f * a * b;
-        System.out.println(c);
+        System.out.println("Hello World");
     }
 }
